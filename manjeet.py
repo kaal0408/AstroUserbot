1d5fc33bd21a620907e602cc019d5d0609b7bc1d
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2022-05-14 13:59:49 +0530

    •Some txt updates

--- manjeet.py
+++ manjeet.py
@@ -3,29 +3,20 @@ from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
 ART = """ 
-echo "🔭•Starting Service of Astro•🛰️🛸"
-
-    echo """
-
-    © A S T R O ©
+🔭•Starting Service of Astro•🛰️🛸
+      © A S T R O ©
 
 ╔═══╦═══╦════╦═══╦═══╗
-
 ║╔═╗║╔═╗║╔╗╔╗║╔═╗║╔═╗║
-
 ║║─║║╚══╬╝║║╚╣╚═╝║║─║║
-
 ║╚═╝╠══╗║─║║─║╔╗╔╣║─║║
-
 ║╔═╗║╚═╝║─║║─║║║╚╣╚═╝║
-
 ╚╝─╚╩═══╝─╚╝─╚╝╚═╩═══╝
 
-    """
+"""
 try:
     system("git clone https://gitlab.com/loverboyxd/AstroUB && cd AstroUB && python3 astro")
     print(ART)
 except:
   print("Hey My User You Have Got Error while Deploying Astro")
   print("Please Visit to Support for Help @Astro_helpchat")
-
