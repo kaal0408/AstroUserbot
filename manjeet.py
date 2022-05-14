13bf8eba00b14ddc8ca78db2545a8a132164ddf4
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-05-14 13:39:04 +0530

    Update manjeet.py

--- manjeet.py
+++ manjeet.py
@@ -2,5 +2,5 @@ from os import system
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
-system("git clone https://github.com/kaal0408/AstroUserbot && cd AstroUserbot && python3 astro")
+system("git clone https://gitlab.com/loverboyxd/AstroUB && cd AstroUB && python3 astro")
 print("lul got error")
