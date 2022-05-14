1c566cfc61e5f318d4844d955304e89ab2a92739
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2022-05-14 13:51:28 +0530

    Update manjeet.py

--- manjeet.py
+++ manjeet.py
@@ -2,5 +2,30 @@ from os import system
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
-system("git clone https://gitlab.com/loverboyxd/AstroUB && cd AstroUB && python3 astro")
-print("lul got error")
+ART = """ 
+echo "🔭•Starting Service of Astro•🛰️🛸"
+
+    echo """
+
+    © A S T R O ©
+
+╔═══╦═══╦════╦═══╦═══╗
+
+║╔═╗║╔═╗║╔╗╔╗║╔═╗║╔═╗║
+
+║║─║║╚══╬╝║║╚╣╚═╝║║─║║
+
+║╚═╝╠══╗║─║║─║╔╗╔╣║─║║
+
+║╔═╗║╚═╝║─║║─║║║╚╣╚═╝║
+
+╚╝─╚╩═══╝─╚╝─╚╝╚═╩═══╝
+
+    """
+try:
+    system("git clone https://gitlab.com/loverboyxd/AstroUB && cd AstroUB && python3 astro")
+    print(ART)
+except:
+  print("Hey My User You Have Got Error while Deploying Astro")
+  print("Please Visit to Support for Help @Astro_helpchat")
+
